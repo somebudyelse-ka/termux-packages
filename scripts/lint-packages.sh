2e923b827cfbda9a377c0b349ec2e4ca5077c58a
somebudyelse-ka <somebudyelse@proton.me>
2025-10-10 16:02:12 -0700

    Update lint-packages.sh
    temporarily change branch name in lint packages to test builds without wasting others' time

--- scripts/lint-packages.sh
+++ scripts/lint-packages.sh
@@ -128,7 +128,7 @@ check_indentation() {
 # - Or specify one of the CI skip tags
 check_version_change() {
 	local base_commit commit_diff package_dir="${1%/*}"
-	base_commit="$(< "$TERMUX_SCRIPTDIR/.git/refs/remotes/origin/master")"
+	base_commit="$(< "$TERMUX_SCRIPTDIR/.git/refs/remotes/origin/add-chawan")"
 
 	[[ -z "$base_commit" ]] && {
 		echo
