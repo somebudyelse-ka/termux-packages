5c01369b3ef27308777a28676282306a2f42937b
somebudyelse-ka <somebudyelse@proton.me>
2026-01-13 00:30:27 +0000

    Attempt 1

---
+++ packages/stockfish/build.sh
@@ -0,0 +1,14 @@
+TERMUX_PKG_HOMEPAGE=https://stockfishchess.org/
+TERMUX_PKG_DESCRIPTION="A free and strong UCI chess engine"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_BUILD_DEPENDS="git, build-essential, clang, make, wget"
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_VERSION=17.1
+TERMUX_PKG_SRCURL=https://github.com/official-stockfish/Stockfish/archive/refs/tags/sf_${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=0cfd9396438798cc68f5c0d5fa0bb458bb8ffff7de06add841aaeace86bec1f1
+
+termux_step_make () {
+    cd src
+    make -j profile-build COMP=clang
+}
