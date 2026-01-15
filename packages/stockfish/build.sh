8a16f8fb8420e384812a8faf05bc48d4b5055f82
somebudyelse-ka <somebudyelse@proton.me>
2026-01-14 18:36:33 -0800

    Maybe this will work

--- packages/stockfish/build.sh
+++ packages/stockfish/build.sh
@@ -14,7 +14,7 @@ termux_step_make () {
 					-e 's/^x86_64$/x86-64/' \
 					-e 's/^i686$/x86-32/')
 	make net
-	if [[$TERMUX_ARCH == "arm"]]; then
+	if [[ "$TERMUX_ARCH" == "arm" ]]; then
 		echo "its arm.sanity check complete."
 		make -j build ARCH=$TARGET_ARCH COMP=ndk CXX=armv7a-linux-androideabi21-clang++
 		make strip ARCH=$TARGET_ARCH COMP=ndk CXX=armv7a-linux-androideabi21-clang++
