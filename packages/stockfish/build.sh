da862c61e0057913b5b25be297c1c6a92b7ee092
somebudyelse-ka <somebudyelse@proton.me>
2026-01-16 15:08:46 -0800

    Test suggested changes

--- packages/stockfish/build.sh
+++ packages/stockfish/build.sh
@@ -9,20 +9,19 @@ TERMUX_PKG_SHA256=0cfd9396438798cc68f5c0d5fa0bb458bb8ffff7de06add841aaeace86bec1
 
 termux_step_make () {
 	cd src
-	TARGET_ARCH=$(echo -n "$TERMUX_ARCH" | sed -e 's/^aarch64$/armv8/' \
-					-e 's/^arm$/armv7/' \
-					-e 's/^x86_64$/x86-64/' \
-					-e 's/^i686$/x86-32/')
+	local TARGET_ARCH
+ 	case "$TERMUX_ARCH" in 
+ 		'aarch64') TARGET_ARCH='armv8';; 
+ 		'arm')     TARGET_ARCH='armv7';; 
+ 		'x86_64')  TARGET_ARCH='x86-64';; 
+ 		'i686')    TARGET_ARCH='x86-32';; 
+ 		*) termux_error_exit "Architecture not supported by build system" 
+ 	esac
 	make net
-	if [[ "$TERMUX_ARCH" == "arm" ]]; then
-		make -j build ARCH=$TARGET_ARCH COMP=ndk CXX=armv7a-linux-androideabi21-clang++
-		make strip ARCH=$TARGET_ARCH COMP=ndk CXX=armv7a-linux-androideabi21-clang++
-	else
-		make -j build ARCH=$TARGET_ARCH COMP=ndk
-		make strip ARCH=$TARGET_ARCH COMP=ndk
-	fi
-}
-
-termux_step_make_install() {
-	install -Dm755 "$TERMUX_PKG_SRCDIR/src/stockfish" "$TERMUX_PREFIX/bin/stockfish"
+	local CXX
+ 	if [[ "$TERMUX_ARCH" == "arm" ]]; then 
+ 		CXX="armv7a-linux-androideabi$TERMUX_PKG_API_LEVEL" 
+ 	fi
+	make -j build ARCH=$TARGET_ARCH COMP=ndk
+	make strip ARCH=$TARGET_ARCH COMP=ndk
 }
